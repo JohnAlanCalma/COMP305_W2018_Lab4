f1d7cf9b49474a90559db56db69910317229526d
johnalanc <johnalan.calma@gmail.com>
2018-02-25 19:31:03 -0500

    Added zoom in / zoom out functionality

---
+++ Assets/Scripts/camera/CameraCloseUpTrigger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCloseUpTrigger : MonoBehaviour {
+
+    public GameObject Player;
+    public Camera mainCamera;
+    public Camera closeUpCamera;
+
+    private PlayerController playerController;
+
+    private bool enterTriggerPosition;
+    private void Start()
+    {
+        playerController = Player.GetComponent<PlayerController>();
+     }
+
+    // runs pn the first frame this object collides with another o jegt
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mainCamera.enabled = false;
+            closeUpCamera.enabled = true;
+            enterTriggerPosition = true;
+        }
+    }
+
+   // runs on the last frame this oject collides with an object leaving it's area
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            mainCamera.enabled = true;
+            closeUpCamera.enabled = false;
+            enterTriggerPosition = false;
+        }
+
+    }
+
+
+    // run on every frame that an object is in the collision area
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (enterTriggerPosition && closeUpCamera.orthographicSize > 0) {
+            closeUpCamera.orthographicSize -= playerController.moveH / 60;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/camera/CameraFollowsFixY.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowsFixY : MonoBehaviour {
+
+     public Transform playerPosition;
+    public float size = 5;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Camera>().orthographicSize = size;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(playerPosition.position.x , this.transform.position.y, this.transform.position.z);
+       System.Console.WriteLine(playerPosition.position.x);
+       // Debug.Log(playerPosition.position.x);
+   //     System.Diagnostics.Debug.WriteLine(playerPosition.position.x);
+
+    }
+}
